447dac6bc8ef0855aad2fb0f83723cc6837302be
MoHaMMeD HaiDeR <103089352+m7hmdd@users.noreply.github.com>
2022-08-31 22:37:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,48 +29,50 @@ def check(text):
       }
       if ":" in text and text.split(":")[0] != '' and text.split(":")[1] != '':
         while True:
-          response = requests.patch(url + method, params=params, data=json.dumps(data)).text
-          # if '"This name is already in use"' in response :
-
-          if '"username":"{}"'.format(username) in response:
-                requests.get("https://api.telegram.org/bot658204306:AAEGf-4x6znalHoDevsMsSr-hi8VHp4nMjU/sendMessage?chat_id=420953620&text=[+]Done Hunted\n— — — —\n[+]Username :: {}\n[+]Requests Number :: {}".format(username, abu_jasim[username]))
-                a = requests.patch(url + method, params=params, data=json.dumps(data)).text
-                print(a)
-                break
-          elif 'Invalid access_token:' in response:
-            break
-          else:
-            abu_jasim[user[0]] += 1
-            print("[+]Username :: {}\n[+]Requests Number :: {}".format(user[0], abu_jasim[user[0]]))
-            # bot.edit_message_text(chat_id=message.chat.id, message_id=infoM.message_id, text="[+]Username :: {}\n[+]Requests Number :: {}".format(username, count))
-pastebin = requests.get("https://pastebin.com/raw/ShhRQVBK").text.splitlines()
-for it in pastebin:
-    threading.Thread(target=check, args=[it.replace("\n",'')]).start()
-bot.send_message(chat_id=420953620,text=f'Started... {str(len(abu_jasim))}')
-def sendd(message):
-    a = []
-    if len(abu_jasim) > 0:
-        for item in abu_jasim:
-            a.append(" {} :: {}\n".format(item, abu_jasim[item]))
-            users = "".join(a[:51])
-            bot.send_message(message.chat.id, text=users)
-    else:
-        bot.send_message(message.chat.id, "[+]Not Found Username ...")
-@bot.message_handler(commands=['check'])
-def Send(message):
-    threading.Thread(target=sendd, args=message).start()
 
 
-            
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
-def redirect_message():
-    json_string = request.get_data().decode("utf-8")
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+#token = "" # файл, содержащий 
+from tambotapi import TamBot
+import tambotapi
+import json, os
+import logging
+from flask import Flask, request, jsonify  # для webhook
 
 
-if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url="https://bottelem.herokuapp.com/" + str(BOT_TOKEN))
-    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# }
+
+token = "U1v2LMDR7nJ9kSU0j0LV9Qd3peQTFaW4-7oCN4zRLxc"
+
+
+bot = TamBot(token)
+logger = tambotapi.logger
+logger.setLevel(logging.DEBUG)
+app = Flask(__name__)  # для webhook
+while True:
+        upd = bot.get_updates()  # получаем внутреннее представление сообщения (контента) отправленного боту (сформированного ботом)
+        # этот способ не формирует событие (mark_seen) о прочтении ботом сообщения, нужно формировать его самостоятельно 
+        if upd:  # основной код, для примера представлен эхо-бот
+            chat_id = bot.get_chat_id(upd)
+            text = bot.get_text(upd)
+            if text == "/start":
+              bot.send_message("Hello", chat_id)
+
+@app.route('/', methods=['POST'])  # для webhook
+def main():
+    while True:
+        upd = bot.get_updates()  # получаем внутреннее представление сообщения (контента) отправленного боту (сформированного ботом)
+        # этот способ не формирует событие (mark_seen) о прочтении ботом сообщения, нужно формировать его самостоятельно 
+        if upd:  # основной код, для примера представлен эхо-бот
+            chat_id = bot.get_chat_id(upd)
+            text = bot.get_text(upd)
+            if text == "/start":
+              bot.send_message("Hello", chat_id)
+            logger.info('Сообщение получено и отправлено')
+        return jsonify(upd)  # для webhook
+
+
+if __name__ == '__main__':  # для webhook
+    try:
+        app.run(port=int(os.environ.get("PORT", 5000)), host="0.0.0.0") # порт нужно выбирать нестандартный для уменьшения количества атак
+    except KeyboardInterrupt:
+        exit()
